16c8b0b4172a1efbe6a4a2fd766d66e3de16ec83
Andrew Cate <andrew92508@gmail.com>
2018-07-15 18:54:18 -0700

    Completed solution for problem 8
    Completed a working version of the solution to problem 8.

--- EulerProject08main.cpp
+++ EulerProject08main.cpp
@@ -1,28 +1,50 @@
 #include <iostream>
-using namespace std;
+#include <fstream>
 #include <vector>
 #include <string>
-#include <fstream>
+
+using namespace std;
 
 int main() {
-	int adjacentInts[13];
-	string tempNums, PEuler08Nums;
-
+	int adjInt[12];
+	long int highestProduct = 0, product = 1;
+	string PE08Nums;
+	
+	//getting 1000 numbers from text file called PEuler08Nums.txt
 	ifstream myfile;
 	myfile.open("PEuler08Nums.txt");
 
-	while (!myfile.eof()) {
-		getline(myfile, tempNums);
-		PEuler08Nums += tempNums;
-		cout << PEuler08Nums << endl;
-	}
+
+	getline(myfile, PE08Nums);
 
 	myfile.close();
-
-	cout << PEuler08Nums << endl;
-
-
+	
+	//filling my vector with opening 13 numbers
+	for(int i = 0; i < 13;i++) {
+		adjInt[i] = PE08Nums.at(i) - 48;
+	}
+	
+	//initial check for the highest product
+	for (int i = 0; i < 13; i++)
+		product *= adjInt[i];
+	highestProduct = product;
+	
+	for (int i = 13; i < PE08Nums.length(); i++) {
+		//shifts numbers in vector to the right by 1
+		for (int j = 0; j < 12; j++)
+			adjInt[j] = adjInt[j + 1];
+		adjInt[12] = PE08Nums.at(i) - 48;
+		
+		//checks for new highest product
+		product = 1;
+		for (int i = 0; i < 13; i++)
+			product *= adjInt[i];
+		if (product > highestProduct)
+			highestProduct = product;
+	}
+	
+	cout << highestProduct << endl;
+	return 0;
 	
 	system("pause");
-	return 0;
 }
